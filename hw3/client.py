958514f82e8619ecad849401cf912c2434e50c9a
skim1225 <trollpotato12@gmail.com>
2025-09-09 17:56:41 -0700

    Completed assignment

--- hw3/client.py
+++ hw3/client.py
@@ -4,16 +4,18 @@
 # 9 September 2025
 
 #Client program to send data to a server via a socket, and then have that same data echoed back.
+
 import socket
+import sys
 
-HOST = "127.0.0.1" #The server's host name or IP address
-PORT = 65432 #The port used by the server
+HOST = "127.0.0.1"
+PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Create a new socket using the given address family, socket
-    #type and protocol number
-    s.connect((HOST, PORT)) #Connect to a remote socket at the address and port
-    s.sendall(b"Hello networking!") #Send data to the socket. The socket must be connected to a remote socket. Data type
-    #is byte
-    data = s.recv(1024) #Receive data from the socket. The return type is a bytes object representing the data received.
-    #The maximum amount of data to be received in each chunk is 1024 as specified.
-    print(f"Received {data!r}") #print to console
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # get terminal input
+    num = int(sys.argv[1])
+    # convert input to bytes and send to server
+    s.sendall(str(num).encode())
+    data = s.recv(1024)
+    print(f"Sent {num} and received {data!r}")
